eee57d990162909cda3771794c5e7ce59d0108a5
abha.a.patil@gmail.com <abhapatil2533@gmail.com>
2026-01-18 16:15:01 +0530

    ignored pkl files and added validation dataset

--- train_model.py
+++ train_model.py
@@ -5,14 +5,19 @@ from sklearn.linear_model import LogisticRegression
 
 # Load dataset
 train_data = pd.read_excel("train.xlsx")
+val_data = pd.read_excel("validation_sample.xlsx")
 
 X = train_data["tweet"] #input                  
 y = train_data["label"]  #output i.e (0, 1, -1)  
+
+X_val = val_data["tweet"]
+y_val = val_data["label"]
 
 # Vectorizer converts the text into numerical format because ML models work with numbers
 vectorizer = TfidfVectorizer(stop_words="english", max_features=5000)   #limits vocabulary, our dataset might have more than 50,000 words
 #this tells the model to remember the top 5000 important words, and stop_words ignores words like and, is, etc which dont hold significance in classification
 X_vectorized = vectorizer.fit_transform(X)
+X_val_vectorized = vectorizer.transform(X_val)
 
 # Model training
 model = LogisticRegression(max_iter=1000)
@@ -21,8 +26,11 @@ model.fit(X_vectorized, y)
 # fit → learns vocabulary from your dataset
 # transform → converts all tweets into numbers
 
-#so pickle files store Python objects exactly as they are in memory
+# checking for overfitting
+print("Training Accuracy:", model.score(X_vectorized, y))
+print("Validation Accuracy:", model.score(X_val_vectorized, y_val))
 
+#so pickle files store Python objects exactly as they are in memory
 joblib.dump(model, "model.pkl")
 joblib.dump(vectorizer, "vectorizer.pkl")
 
